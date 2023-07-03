96d15f687b487880f05d689efdf5d76e5da910b6
Владислав <vlados66625@mail.ru>
2023-07-03 13:34:52 +0300

    проект на защиту

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+let navPage = document.querySelector('.header__navigation-page');
+let navToggle = document.querySelector('.navigation-user__toggle');
+
+navPage.classList.remove('header-top__main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navPage.classList.contains('navigation-page--closed')) {
+    navPage.classList.remove('navigation-page--closed');
+    navPage.classList.add('navigation-page--opened');
+    navToggle.classList.remove('navigation-user__toggle--closed');
+    navToggle.classList.add('navigation-user__toggle--opened');
+  } else {
+    navPage.classList.add('navigation-page--closed');
+    navPage.classList.remove('navigation-page--opened');
+    navToggle.classList.add('navigation-user__toggle--closed');
+    navToggle.classList.remove('navigation-user__toggle--opened');
+  }
+});
